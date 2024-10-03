795d6fdb89745ae772b1808160e3ab010b35394a
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-10-03 23:51:49 +0300

    products' entity

---
+++ app/src/main/java/com/senseicoder/quickcart/core/entity/product/Product.kt
@@ -0,0 +1,32 @@
+package com.senseicoder.quickcart.core.entity.product
+
+import androidx.room.PrimaryKey
+import com.senseicoder.quickcart.core.entity.brand.Image
+import com.senseicoder.quickcart.core.model.DisplayProduct
+
+data class Product(
+    @PrimaryKey
+    val id: Long,
+    val title: String,
+    val body_html: String,
+    val vendor: String,
+    val product_type: String,
+    val created_at: String,
+    val handle: String,
+    val updated_at: String,
+    val published_at: String,
+    val template_suffix: Any,
+    val status: String,
+    val published_scope: String,
+    val tags: String,
+    val admin_graphql_api_id: String,
+    val variants: List<Variant>,
+    val options: List<Option>,
+    val images: List<Images>,
+    val image: Image,
+)
+
+fun Product.mapRemoteProductToDisplayProduct(): DisplayProduct {
+    return DisplayProduct(this.id,this.variants[0].price, this.product_type,this.title ?: "", this.image?.src ?: "", this.tags)
+}
+
---
+++ app/src/main/java/com/senseicoder/quickcart/core/entity/product/ProductResponse.kt
@@ -0,0 +1,3 @@
+package com.senseicoder.quickcart.core.entity.product
+
+data class ProductResponse(val products: List<Product>)
