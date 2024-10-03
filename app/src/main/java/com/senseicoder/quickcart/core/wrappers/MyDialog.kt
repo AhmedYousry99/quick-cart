0ed52fc30e1b03983b98e65aec0582be626117ef
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-10-03 23:53:36 +0300

    utils

---
+++ app/src/main/java/com/senseicoder/quickcart/core/wrappers/MyDialog.kt
@@ -0,0 +1,34 @@
+package com.senseicoder.quickcart.core.wrappers
+
+import android.app.AlertDialog
+import android.content.Context
+
+class MyDialog {
+    fun showAlertDialog(message: String,context: Context) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    fun showAlertDialog(message: String, context: Context, callback: (Boolean) -> Unit) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+            .setPositiveButton("OK") { dialog, _ ->
+                callback(true)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                callback(false)
+                dialog.dismiss()
+            }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+
+
+}
