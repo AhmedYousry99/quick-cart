d4315499776b825219b23de1d76c49acaad127bc
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2024-10-07 02:20:05 +0300

    add line to hide toolbar

--- app/src/main/java/com/senseicoder/quickcart/features/main/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/senseicoder/quickcart/features/main/ui/profile/ProfileFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.senseicoder.quickcart.R
+import com.senseicoder.quickcart.core.dialogs.ConfirmationDialog
+import com.senseicoder.quickcart.core.dialogs.ConfirmationDialogFragment
+import com.senseicoder.quickcart.core.enums.DialogType
 import com.senseicoder.quickcart.databinding.FragmentProfileBinding
+import com.senseicoder.quickcart.features.main.MainActivity
 
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
     private lateinit var binding: FragmentProfileBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +24,32 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        (requireActivity() as MainActivity).toolbarVisibility(false)
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.apply {
+            btnSettings.setOnClickListener {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_profileFragment_to_settingsFragment)
+            }
+            btnOrderHistory.setOnClickListener {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_profileFragment_to_orderFragment)
+            }
+            btnHowToUse.setOnClickListener {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_profileFragment_to_howToUseFragment)
+            }
+            btnLogOut.setOnClickListener {
+                ConfirmationDialogFragment(DialogType.LOGOUT) {
+                    //TODO: logout
+                }.show(childFragmentManager, null)
+            }
+        }
     }
 }
