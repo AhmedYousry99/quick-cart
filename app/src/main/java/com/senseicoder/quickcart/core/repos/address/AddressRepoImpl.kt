7dcd767c8e02919abf1c5fb4b4c6471eb534d34c
EiiiDOo <ahmed.eid159753@gmail.com>
2024-10-09 13:24:33 +0300

    add function to address from remote and create repo for address

---
+++ app/src/main/java/com/senseicoder/quickcart/core/repos/address/AddressRepoImpl.kt
@@ -0,0 +1,73 @@
+package com.senseicoder.quickcart.core.repos.address
+
+import com.senseicoder.quickcart.core.global.Constants
+import com.senseicoder.quickcart.core.model.AddressOfCustomer
+import com.senseicoder.quickcart.core.model.fromEdges
+import com.senseicoder.quickcart.core.model.fromNodes
+import com.senseicoder.quickcart.core.network.interfaces.StorefrontHandler
+import com.senseicoder.quickcart.core.services.SharedPrefs
+import com.storefront.CustomerAddressesQuery
+import com.storefront.type.MailingAddressInput
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class AddressRepoImpl(val remote: StorefrontHandler, val local: SharedPrefs) : AddressRepo {
+
+    private var token =
+        local.getSharedPrefString(Constants.USER_TOKEN, Constants.USER_TOKEN_DEFAULT)
+
+    override suspend fun getCustomerAddresses(): Flow<CustomerAddressesQuery.Customer?> {
+        updateToken()
+        return flow {
+            remote.getCustomerAddresses(token).collect{
+                emit(it)
+            }
+        }
+    }
+
+    override suspend fun updateCustomerAddress(
+        address: MailingAddressInput,
+        id: String
+    ): Flow<String?> {
+        updateToken()
+        return  flow {
+            remote.updateCustomerAddress(address,token,id).collect{
+                emit(it)
+            }
+        }
+    }
+
+    override suspend fun deleteAddress(id: String): Flow<String?> {
+        updateToken()
+        return flow {
+            remote.deleteAddress(id,token).collect{
+                emit(it)
+            }
+        }
+    }
+
+    override suspend fun createAddress(
+        customerAddress: MailingAddressInput
+    ): Flow<String?> {
+        updateToken()
+        return flow {
+            remote.createAddress(customerAddress,token).collect{
+                emit(it)
+            }
+        }
+    }
+
+    override suspend fun updateDefaultAddress(id: String): Flow<List<AddressOfCustomer>> {
+        updateToken()
+        return flow {
+            remote.updateDefaultAddress(token,id).collect{
+                emit(it.fromNodes())
+            }
+        }
+    }
+
+    override fun updateToken() {
+        token = local.getSharedPrefString(Constants.USER_TOKEN, Constants.USER_TOKEN_DEFAULT)
+    }
+
+}
