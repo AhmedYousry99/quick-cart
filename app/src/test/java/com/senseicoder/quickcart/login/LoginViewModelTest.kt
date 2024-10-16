813d43868f5d72a57b659620f5bf10d7bd449a21
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-10-16 22:39:43 +0300

    updating methods

--- app/src/test/java/com/senseicoder/quickcart/login/LoginViewModelTest.kt
+++ app/src/test/java/com/senseicoder/quickcart/login/LoginViewModelTest.kt
@@ -79,39 +79,4 @@ class LoginViewModelTest {
         assertEquals("Login failed", errorMessage)
     }
 
-//    @Test
-//    fun `signupAsGuest should emit Success state when signup is successful`() = runTest {
-//        // Arrange
-//        val fakeCustomerRepo = FakeCustomerRepo()
-//        val viewModel = LoginViewModel(fakeCustomerRepo)
-//
-//        // Act: Call the signup function
-//        viewModel.signupAsGuest()
-//
-//        // Collect the login state in a separate coroutine
-//        val job = launch {
-//            viewModel.loginState.collect { result ->
-//                // Assert: Expecting loading state initially
-//                if (result is ApiState.Loading) {
-//                    assertEquals(ApiState.Loading, result)
-//                }
-//                // Assert: Expecting success state after processing
-//                if (result is ApiState.Success) {
-//                    val customerData = result.data
-//                    assertEquals("fake_guest_user_id", customerData.id)
-//                    assertEquals("fake_guest_user_token", customerData.token)
-//                    assertEquals("guest@example.com", customerData.email)
-//                    assertEquals("Guest User", customerData.displayName)
-//                    assertEquals("fake_guest_cart_id", customerData.cartId)
-//                }
-//            }
-//        }
-//
-//        // Advance until idle to process emissions
-//        advanceUntilIdle()
-//
-//        // Cancel the job to stop collecting
-//        job.cancel()
-//    }
-
 }
